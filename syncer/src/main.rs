a74995a1339599a536e9f0478b1c0c13d53655d9
Nick Hynes <nick@enshrine.ai>
2023-09-14 02:18:53 +0000

    Download on conflict, too

--- syncer/src/main.rs
+++ syncer/src/main.rs
@@ -7,23 +7,80 @@ async fn main() {
     let username = std::env::var("ANKI_USERNAME").expect("ANKI_USERNAME not set");
     let password = std::env::var("ANKI_PASSWORD").expect("ANKI_PASSWORD not set");
 
+    let authenticate = |endpoint: String| async {
+        anki::sync::login::SyncAuth {
+            endpoint: endpoint.parse().ok(),
+            ..anki::sync::login::sync_login(
+                &username,
+                &password,
+                Some(endpoint),
+                Default::default(),
+            )
+            .await
+            .expect("login failed")
+        }
+    };
+    let reauthenticate = |prior: anki::sync::login::SyncAuth, new_endpoint: Option<String>| async {
+        if let Some(new_endpoint) = new_endpoint {
+            authenticate(new_endpoint).await
+        } else {
+            prior
+        }
+    };
+
     let mut collection = anki::collection::CollectionBuilder::new(coll_path.clone())
-        .set_no_prepopulate(true) // Don't create default decks and notetypes or the sanity check will fail.
         .build()
         .expect("failed to build collection");
 
-    let sync_auth = anki::sync::login::sync_login(username, password, Some(host), Default::default())
-        .await
-        .expect("login failed");
+    let sync_auth = authenticate(host).await;
 
-    collection
-        .normal_sync(sync_auth, Default::default())
+    match collection
+        .normal_sync(sync_auth.clone(), Default::default())
         .await
-        .expect("failed to sync");
+    {
+        Ok(anki::sync::collection::normal::SyncOutput {
+            required: anki::sync::collection::normal::SyncActionRequired::FullSyncRequired { .. },
+            new_endpoint,
+            ..
+        }) => download(collection, reauthenticate(sync_auth, new_endpoint).await).await,
+        Ok(_) => drop(collection),
+        Err(anki::error::AnkiError::SyncError {
+            source:
+                anki::error::SyncError {
+                    kind:
+                        anki::error::SyncErrorKind::Conflict
+                        | anki::error::SyncErrorKind::ResyncRequired
+                        | anki::error::SyncErrorKind::DatabaseCheckRequired,
+                    ..
+                },
+        }) => {
+            let local = collection
+                .sync_meta()
+                .expect("failed to get local sync meta");
+            let mut sync_client =
+                anki::sync::http_client::HttpSyncClient::new(sync_auth.clone(), Default::default());
+            let status =
+                anki::sync::collection::status::online_sync_status_check(local, &mut sync_client)
+                    .await
+                    .expect("failed to online status check");
+            download(
+                collection,
+                reauthenticate(sync_auth, status.new_endpoint).await,
+            )
+            .await;
+        }
+        Err(e) => panic!("failed to normal sync: {:?}", e),
+    }
 
     // Re-open the collection again to upgrade schema.
-    drop(collection);
     anki::collection::CollectionBuilder::new(coll_path)
         .build()
         .expect("failed to re-build collection");
+}
+
+async fn download(collection: anki::collection::Collection, auth: anki::sync::login::SyncAuth) {
+    collection
+        .full_download(auth, Default::default())
+        .await
+        .expect("full download failed");
 }
